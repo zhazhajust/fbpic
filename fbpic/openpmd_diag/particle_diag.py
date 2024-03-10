0ee8629e0242092f2d0af4079d19faa4dc6c1fc3
Cai Jie <jiecai@stu.pku.edu.cn>
2024-03-10 19:48:05 +0800

    add uniform stride select

--- fbpic/openpmd_diag/particle_diag.py
+++ fbpic/openpmd_diag/particle_diag.py
@@ -396,9 +396,6 @@ class ParticleDiagnostic(OpenPMDDiagnostic) :
         # subsampling selector
         if self.subsampling_uniform_stride is not None :
             assert isinstance(self.subsampling_uniform_stride, int)
-            # subsampling_array = np.zeros_like(select_array, dtype='bool')
-            # subsampling_array[::self.subsampling_uniform_stride] = True
-            # select_array = np.logical_and(subsampling_array,select_array)
             cur_pid = species.tracker.id
             bool_select = (cur_pid % self.subsampling_uniform_stride == 0)
             select_array = np.logical_and(bool_select,select_array)
